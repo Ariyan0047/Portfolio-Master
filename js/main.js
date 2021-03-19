13ac7ac0ebe86386bf93c3a2a2718d16999026b6
Ariyan0047 <aariyan0047@gmail.com>
2021-03-19 10:03:49 +0600

    v.21

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const menu = document.querySelector(".hamburger-menu");
 const nav = document.querySelector(".navbar");
+const submit = document.querySelector("#submit");
 
 menu.addEventListener("click", () => {
   nav.classList.toggle("change");
+});
+
+submit.addEventListener("click", () => {
+  alert("Form submission is under construction !!");
 });
